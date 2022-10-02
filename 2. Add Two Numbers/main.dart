a1e40aaabb8276910dfe6d2bb76eea0e1df05a5a
Giuliano Stravini <giu.stravini@hotmail.com>
2022-10-01 21:19:10 -0300

    primeiro commit

---
+++ 2. Add Two Numbers/main.dart
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+void main() {
+  ListNode l1 = [2, 4, 3];
+  ListNode l2 = [5, 6, 4];
+
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {}
+}
+
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
